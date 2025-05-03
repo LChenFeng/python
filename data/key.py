a3b64d3e677195e4d4abb8a04081c691e47b5df7
LChenFeng <129513503+LChenFeng@users.noreply.github.com>
2025-05-03 23:53:25 +0800

    Add files via upload

---
+++ data/key.py
@@ -0,0 +1,19 @@
+#前置颜色
+front=(200,200,200)
+
+#背景颜色
+ground=(128,128,128)
+
+#字体
+font='KaiTi'
+
+#宋体 SimSun
+#黑体 SimHei
+#微软雅黑 Microsoft YaHei
+#微软正黑体 Microsoft JhengHei
+#新宋体 NSimSun
+#标楷体 DFKai-SB
+#仿宋 FangSong
+#楷体 KaiTi
+#仿宋_GB2312 FangSong_GB2312
+#楷体_GB2312 KaiTi_GB2312
